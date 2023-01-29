86ff6746a787574768394fe0184326e726cd0127
Sitt Aung <sittaung07@gmail.com>
2023-01-29 11:41:18 +0630

    Recursively displaying categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ $categories }}
-                </div>
+            <div class="overflow-hidden p-6 grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+                @foreach ($categories as $category)
+                    <x-category :category="$category" />
+                @endforeach
             </div>
         </div>
     </div>
