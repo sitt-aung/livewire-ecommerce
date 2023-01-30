4a35b6fe6c8c4b4b75fc791dde9a736aa3896863
Sitt Aung <sittaung07@gmail.com>
2023-01-30 10:05:08 +0630

    Showing a product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductShowController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomeController::class);
+
+Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
