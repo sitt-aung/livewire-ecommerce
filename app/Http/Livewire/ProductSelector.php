2a3ed53ba7c1c0597db65d02c8e59fb36a611195
Sitt Aung <sittaung07@gmail.com>
2023-01-30 11:25:12 +0630

    Faking adding the final variation

--- app/Http/Livewire/ProductSelector.php
+++ app/Http/Livewire/ProductSelector.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Variation;
 use Livewire\Component;
 
 class ProductSelector extends Component
 {
     public $product;
     public $initialVariations;
+    public $skuVariant;
+
+    protected $listeners = [
+        'skuVariantSelected'
+    ];
 
     public function mount()
     {
         $this->initialVariations = $this->product->variations->sortBy('order')->groupBy('type')->first();
+    }
+
+    public function skuVariantSelected($variantId)
+    {
+        if (!$variantId) {
+            $this->skuVariant = null;
+            return;
+        }
+
+        $this->skuVariant = Variation::find($variantId);
+    }
+
+    public function addToCart()
+    {
+        dd($this->skuVariant);
     }
 
     public function render()
--- resources/views/livewire/product-selector.blade.php
+++ resources/views/livewire/product-selector.blade.php
@@ -1,5 +1,15 @@
-<div>
+<div class="space-y-6">
     @if ($initialVariations)
         <livewire:product-dropdown :variations="$initialVariations" />
+    @endif
+
+    @if ($skuVariant)
+        <div class="space-y-6">
+            <div class="font-semibold text-lg">
+                {{ $skuVariant->formattedPrice() }}
+            </div>
+
+            <x-primary-button wire:click.prevent="addToCart">Add to cart</x-primary-button>
+        </div>
     @endif
 </div>
